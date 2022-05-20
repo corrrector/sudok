07cc668ff2d441c97b17437fbef833cbadc93219
Yury <businesselbgus@mail.ru>
2022-05-20 14:48:27 +0300

    first change

--- sudoku.js
+++ sudoku.js
@@ -4,15 +4,21 @@
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 function solve(boardString) {
-  console.log('first changes');
-}
+    let y = boardString.split("").map((el)=>el == '-' ? 0 : + el)
+  let g = []
+    for (let i=0; i<81; i+=9){
+   g.push(y.slice(i,i+9))
+    }
+  return g
+  }
+
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
 function isSolved(board) {
-
+return true
 }
 
 // Takes in a board in some form and
@@ -21,7 +27,7 @@ function isSolved(board) {
 // The input board will be in whatever
 // form `solve` returns.
 function prettyBoard(board) {
-
+console.table(board)
 }
 
 // Exports all the functions to use them in another file.
