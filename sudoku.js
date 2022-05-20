98a979bdf1dce0fe6cb4c1f78969ea4792c11125
mac <mac@MacBook-Pro-mac.local>
2022-05-20 16:15:47 +0300

    rel1

--- sudoku.js
+++ sudoku.js
@@ -9,6 +9,59 @@ function solve(boardString) {
   for (let i = 0; i < 81; i += 9) {
     board.push(arr.slice(i, i + 9));
   }
+  function cord(arr) {
+    for (let x = 0; x < arr.length; x++) {
+      for (let y = 0; y < arr.length; y++) {
+        if (arr[x][y] === 0) {
+          return [x, y];
+        }
+      }
+    }
+    return 'end';
+  }
+  function tripleСheck(board, cord, n) {
+    let [x, y] = cord
+    function vertical(board) {
+      for (let i = 0; i < 9; i ++) {
+        if (board[i][x] === n) {
+            return false;
+        }
+    }
+  return true
+  }
+    function horizontal(board) {
+      for (let i = 0; i < 9; i ++) {
+        if (board[y][i] === n) {
+            return false;
+        }
+    }
+  return true
+  }
+
+    function cube() {
+   return true
+    }
+  return vertical(board) && horizontal(board) && cube()
+  }
+  function test() {
+    if (cord(board) === 'end') {
+      return true;
+    }
+    for (let i = 0; i < 81; i++) {
+      const num = Math.floor(Math.random() * (10 - 1) + 1);
+      if (!tripleСheck) {
+        const [x, y] = cord;
+        board[x][y] = num;
+        if(test()) {
+          return true
+        }
+        sudoku[cord] = 0
+      }
+    
+      }
+    }
+  }
+  test();
   return board;
 }
 
@@ -17,7 +70,7 @@ function solve(boardString) {
 // The input board will be in whatever
 // form `solve` returns.
 function isSolved(board) {
-}dgfbhvgdf
+}
 
 // Takes in a board in some form and
 // returns a String that's well formatted
